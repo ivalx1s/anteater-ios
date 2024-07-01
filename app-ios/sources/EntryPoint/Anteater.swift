00dcaeffe16956a2121a0a325a917499b375d8c6
Ivan Oparin <i.oparin@unitea.com>
2024-07-01 23:43:16 +0400

    Setup initial project structure

---
+++ app-ios/sources/EntryPoint/Anteater.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct Anteater: App {
+	var body: some Scene {
+		WindowGroup {
+			EntryPoint.ContentContainer()
+		}
+	}
+}
+
---
+++ appex-widget/AppexWidget/AnteaterBundle.swift
@@ -0,0 +1,9 @@
+import WidgetKit
+import SwiftUI
+
+@main
+struct AnteaterWidgetBundle: WidgetBundle {
+    var body: some Widget {
+		AnteaterWidget()
+    }
+}
